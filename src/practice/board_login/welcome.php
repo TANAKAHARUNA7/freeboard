7cbb6165bb1c25ff7b2abf0c250f177887cf4361
haruna <haruna0306@g.yju.ac.kr>
2025-08-28 14:28:51 +0900

    welcome

--- src/practice/board_login/welcome.php
+++ src/practice/board_login/welcome.php
@@ -2,10 +2,10 @@
 session_start();
 // 세션을 통해 로그인 여부 확인
 // 로그인된 사용자의 이름 표시 & 환영 메시지 출력
-if (isset($_SESSION["userid"]) && isset($_SESSION["name"])) {
+if (isset($_SESSION["username"]) && isset($_SESSION["name"])) {
     echo "환영합니다! " . htmlspecialchars($_SESSION["name"]) . "님✨";
 } else {
-    $_SESSION["erro"] = "로그인 해주세요.";
+    $_SESSION["error"] = "로그인 해주세요.";
     header("Location: login.php");
     exit;
 }
